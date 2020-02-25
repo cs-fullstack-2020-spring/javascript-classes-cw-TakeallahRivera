fdaec38e6e172ae6e2044e14df0cc0a41b34e18b
Takeallah <takeallahsrivera@icloud.com>
2020-02-25 14:46:34 -0600

    PushTwo

--- classwork.js
+++ classwork.js
@@ -6,6 +6,7 @@ the class and use the function.*/
 
 //Create a GitHubRepository Class//
 
+/*
 class GitHubRepository {
     constructor(userName, fileName, descriptionOfRepository, code) {
         this.userName = userName;
@@ -18,9 +19,43 @@ class GitHubRepository {
     }
     
 }
+//created an objected//
 let newGitHubRepository = new GitHubRepository("Takeallah", "Roxanne", "Devere", "Hendrix");
 
-
+//Printed using function//
 newGitHubRepository.printGitHub();
+
+*/
+
+/*Problem 2:
+Create a Movie class with the following properties: movieName, rating, and yearReleased. Create a method to change the rating and another one to change the yearReleased properties using prompt. Create an object of the class and use the two methods you created.*/
+
+class Movie {
+    constructor(movieName, rating, yearReleased) {
+        this.movieName = "Halloween";
+        this.rating = 5;
+        this.yearReleased = 1978;
+    }
+
+    addNewRating (){
+        let newDamnInput = prompt("Enter a new rating damn it!")
+        this.rating = newDamnInput;
+    }
+
+    addNewYear (){
+        let newDamnInput2 = prompt("Enter a new year released damn it")
+        this.yearReleased = newDamnInput2;
+    }
+
+    printMovieHub() {
+        console.log(`movieName: ${this.movieName}\nrating: ${this.rating}\nyearReleased: ${this.yearReleased}`)
+    }
+
+
+   
+}
+
+
+
 
 
