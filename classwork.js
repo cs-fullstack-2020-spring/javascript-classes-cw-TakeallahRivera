4dd6b1b5862267b0094cb27a5e9a758bacf1dc79
Takeallah <takeallahsrivera@icloud.com>
2020-02-25 14:57:32 -0600

    PushThree

--- classwork.js
+++ classwork.js
@@ -55,6 +55,25 @@ class Movie {
    
 }
 
+let newMovieThings = ("Leprechaun", "9", "1990");
+
+newMovieThings.printMovieHub();
+
+/*### Problem 3:
+Create a Student class with the following properties : studentName, codeSchoolCohort, and grades(grades should be an array of objects and each object should have the properties assignemtName and assignemtnScore). Create a method that accepts a student name and updates the studentName of an instance of your Student Class. Create a method that accepts an assignment name and assignment score and adds a grade object to the grades array. Create two instances of this class with empty grade arrays. Update the name of one instance of the Student Class using the class method. Add one grade object to each instance of the Student Class grade array using the class method.
+
+HINT : use object literal notation to create a grade object in the grade array for each instance of the Student Class
+
+*/
+
+class Student {
+    this.studentName = studentName;
+    this.codeSchoolCohort = codeSchoolCohort;
+    this.grade = grade;
+}
+
+
+
 
 
 
